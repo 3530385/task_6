279ba688ecd2ba34247e5e071d5e0d238710a1da
Евгений Шевченко <3530385@gmail.com>
2022-10-29 22:36:26 +0300

    tests

--- database/test_parser.py
+++ database/test_parser.py
@@ -3,4 +3,4 @@ from parser import get_data_from_avito
 def test_avito():
     asd=2
     assert get_data_from_avito()=='data'
-    assert get_data_from_avito()=='data'
+    assert get_data_from_avito()=='data' # loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong
